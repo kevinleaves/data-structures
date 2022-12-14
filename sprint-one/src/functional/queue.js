1d13d889ff7911d5221de12031c53efdf9ad8320
Kevin Le <iamkevinvle@gmail.com>
2022-12-14 14:52:59 -0800

    implement stack & queue using functional pattern

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,42 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  //create a pointer that starts at the beginning
+  var start = 0
+  var end = 0
+
+  // storage.start = start;
+  // storage.end = end;
 
   // Implement the methods below
-
   someInstance.enqueue = function(value) {
+    //creates numeric keys in storage
+    storage[end] = value;
+    //increment end
+    end++
   };
 
   someInstance.dequeue = function() {
+    for (var key in storage) {
+      if (parseInt(key) === start) {
+        //store deleted value
+        var res = storage[key];
+        delete storage[key]
+      }
+    }
+
+    //increment start to keep track of the next item to dequeue, prevents dequeue from affecting size if queue is empty
+    if (end > start) {
+      start++
+    }
+
+    //return deleted value
+    return res
   };
 
   someInstance.size = function() {
+    //returns distance between 2 pointers
+    return end - start
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,16 +3,50 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  //storage needs to include a counter property
+  storage.counter = 0;
 
+  var index = 0
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[index] = value;
+    index++
+    storage.counter++
+
+  //key is counter: value is the pushed value
+  //finds largest number key and
+  //increases counter by 1
   };
 
   someInstance.pop = function() {
+  //removes largest number key
+  //decreases counter
+    for (var key in storage) {
+      console.log(key, 'key', index, 'idx', storage)
+      if (parseInt(key) === index - 1) {
+        var res = storage[key]
+        delete(storage[key])
+      }
+    }
+
+    if (index > 0 && storage.counter > 0) {
+      index--
+      storage.counter--
+    }
+
+    return res
   };
 
   someInstance.size = function() {
+    //returns current counter in storage
+    return storage.counter;
   };
+
 
   return someInstance;
 };
+
+
+// var myStack = Stack()
+// myStack.push('5') //storage {'0': '5'}
+
