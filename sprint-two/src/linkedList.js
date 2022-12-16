bd9c073db7c8217ee9d11f6ce00cdb143d4c83b8
Kevin Le <iamkevinvle@gmail.com>
2022-12-15 16:35:21 -0800

    implement linkedList

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -3,13 +3,52 @@ var LinkedList = function() {
   list.head = null;
   list.tail = null;
 
+  // I: value passed in from call
+  // O: n/a
+  // C: n/a
+  // E: if list is empty or has 1 node
+
+  // Creating a new node with a given value, adding a pointer from the node that was at
+  // the end to point at this new node, and moves tail pointer to point at new node.
+
   list.addToTail = function(value) {
+    //create the new tail node
+    var newTail = Node(value);
+
+    //if the list is empty, the first node added is both the head and the tail
+    if (list.head === null) {
+      list.head = newTail;
+    } else {
+      list.tail.next = newTail;
+    }
+
+    //change linked list property tail to new tail
+    list.tail = newTail;
   };
 
   list.removeHead = function() {
+    //store value of head in a variable before we remove it
+    var result = list.head.value;
+    // move pointer of head to next node
+    list.head = list.head.next;
+    // return stored value
+    return result;
   };
 
   list.contains = function(target) {
+    // traverse linked list starting from the head
+    var currNode = list.head;
+    while (currNode) {
+      // check current node's value, if it's equal to target
+      if (currNode.value === target) {
+        // return true
+        return true;
+      }
+      // else currNode changes to next node
+      currNode = currNode.next;
+    }
+    // return false
+    return false;
   };
 
   return list;
@@ -27,3 +66,7 @@ var Node = function(value) {
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
+// var LinkedList = LinkedList()
+// LinkedList.addToTail(5)
+// LinkedList.addToTail(5)
