160e98e76e3cde885d35758591c1a754e9d1fc8d
Kevin Le <iamkevinvle@gmail.com>
2024-02-16 14:22:48 -0800

    ll redo

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -1,21 +1,42 @@
-var LinkedList = function() {
+var LinkedList = function () {
   var list = {};
   list.head = null;
   list.tail = null;
 
-  list.addToTail = function(value) {
+  list.addToTail = function (value) {
+    let node = Node(value);
+    // edge case empty list, head becomes tail
+    if (!list.head) {
+      list.head = node;
+    } else {
+      list.tail.next = node;
+    }
+    list.tail = node;
   };
 
-  list.removeHead = function() {
+  list.removeHead = function () {
+    // remove head call on an empty list?
+    const prevHead = list.head;
+    list.head = list.head.next;
+    prevHead.next = null;
+    return prevHead.value;
   };
 
-  list.contains = function(target) {
+  list.contains = function (target) {
+    let curr = list.head;
+    while (curr) {
+      if (target === curr.value) {
+        return true;
+      }
+      curr = curr.next;
+    }
+    return false;
   };
 
   return list;
 };
 
-var Node = function(value) {
+var Node = function (value) {
   var node = {};
 
   node.value = value;
